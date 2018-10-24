91426b5b02ad2a8c326db47ac8173bf60dda3773
dkellam9 <dkellam9@gmail.com>
2018-10-24 16:00:13 -0700

    fully functional, pre-stretch goals

--- taxi_service.rb
+++ taxi_service.rb
@@ -36,7 +36,10 @@ class TaxiService
 
   def assignRider
     while @riderQueue.length > 0
-      if @taxiQueue[0].pickup(@riderQueue[0])
+      if @taxiQueue.length == 0
+        p "I came here to send Taxies and pick up riders... and I'm all outta Taxies"
+      break
+      elsif @taxiQueue[0].pickup(@riderQueue[0])
         @riderQueue.shift()
       elsif
         @taxiQueue.shift()
@@ -52,20 +55,20 @@ taxi_service.addRider("Katherine Halsey")
 taxi_service.addRider("Jacob Keyes")
 taxi_service.addRider("Noble 6")
 
-taxi0 = Taxi.new("Literally the guy from Taxi Driver", 1, "Checker", "A10", "TLKN2ME")
-taxi1 = Taxi.new("James Kirk", 1, "Chevrolet", "Corvette", "CPT LOG")
-taxi2 = Taxi.new("Miranda Keyes", 1, "UNSC", "In Amber Clad", "002552")
-taxi3 = Taxi.new("JC Denton", 1, "Page Ind.", "Something", "A BOMB")
-taxi4 = Taxi.new("John Smith", 1, "Honda", "Odyssey", "2BORING")
+taxi0 = Taxi.new("Literally the guy from Taxi Driver", 4, "Checker", "A10", "TLKN2ME")
+# taxi1 = Taxi.new("James Kirk", 1, "Chevrolet", "Corvette", "CPT LOG")
+# taxi2 = Taxi.new("Miranda Keyes", 1, "UNSC", "In Amber Clad", "002552")
+# taxi3 = Taxi.new("JC Denton", 1, "Page Ind.", "Something", "A BOMB")
+# taxi4 = Taxi.new("John Smith", 1, "Honda", "Odyssey", "2BORING")
 
 taxi_service.addTaxi(taxi0)
-taxi_service.addTaxi(taxi1)
-taxi_service.addTaxi(taxi2)
-taxi_service.addTaxi(taxi3)
-taxi_service.addTaxi(taxi4)
+# taxi_service.addTaxi(taxi1)
+# taxi_service.addTaxi(taxi2)
+# taxi_service.addTaxi(taxi3)
+# taxi_service.addTaxi(taxi4)
 
 p taxi_service.assignRider
-p taxi0
-p taxi_service.riderQueue
-p taxi1
-p taxi2
+# p taxi0
+# p taxi_service.riderQueue
+# p taxi1
+# p taxi2
